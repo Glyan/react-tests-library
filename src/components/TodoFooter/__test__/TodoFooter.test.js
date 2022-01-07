6e9e3ff4a2d5ee9962665f81817b635496668b40
Glyan <pamela27jr@gmail.com>
2022-01-07 18:31:24 +0000

    Using Describe Block

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -10,21 +10,22 @@ const MockTodoFooter = ({numberOfIncompleteTasks}) => {
     )
 }
 
-test ('should render the correct amount of incomplete tasks', async () => {
-    render(<MockTodoFooter numberOfIncompleteTasks={5}/>);
-    const paragraphElement = screen.getByText(/5 tasks left/i);
-    expect(paragraphElement).toBeInTheDocument();
-})
-
-
-test ('should render "task" when the number of imcomplete tasks is 1', async () => {
-    render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
-    const paragraphElement = screen.getByText(/1 task left/i);
-    expect(paragraphElement).toBeInTheDocument();
-})
-
-test ('should be visible', async () => {
-    render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
-    const paragraphElement = screen.getByText(/1 task left/i);
-    expect(paragraphElement).toBeVisible();
+describe ("TodoFooter", () => {
+    test ('should render the correct amount of incomplete tasks', async () => {
+        render(<MockTodoFooter numberOfIncompleteTasks={5}/>);
+        const paragraphElement = screen.getByText(/5 tasks left/i);
+        expect(paragraphElement).toBeInTheDocument();
+    })
+    
+    test ('should render "task" when the number of imcomplete tasks is 1', async () => {
+        render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
+        const paragraphElement = screen.getByText(/1 task left/i);
+        expect(paragraphElement).toBeInTheDocument();
+    })
+    
+    test ('should be visible', async () => {
+        render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
+        const paragraphElement = screen.getByText(/1 task left/i);
+        expect(paragraphElement).toBeVisible();
+    })
 })
