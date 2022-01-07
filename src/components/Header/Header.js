a9791d331da67694d72ea6514136a7d730036aa1
Glyan <pamela27jr@gmail.com>
2022-01-07 18:28:10 +0000

    Trying different assertions for TodoFooter component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,5 @@ export default function Header({ title }) {
     return (
     <>
         <h1 className="header">{title}</h1>
-        <h3 className="header">Cats</h3>
     </>
-    )}
+)}
---
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -0,0 +1,30 @@
+import { render, screen} from '@testing-library/react';
+import TodoFooter from '../TodoFooter';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+
+const MockTodoFooter = ({numberOfIncompleteTasks}) => {
+    return (
+        <BrowserRouter>
+            <TodoFooter numberOfIncompleteTasks={numberOfIncompleteTasks}/>
+        </BrowserRouter>
+    )
+}
+
+test ('should render the correct amount of incomplete tasks', async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={5}/>);
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+})
+
+
+test ('should render "task" when the number of imcomplete tasks is 1', async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+})
+
+test ('should be visible', async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeVisible();
+})
